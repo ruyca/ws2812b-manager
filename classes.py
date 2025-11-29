7c56f497c671b6846d0758008126f058c4ded84a
ruyca <ruycabello@comunidad.unam.mx>
2025-11-29 15:51:12 -0600

    Added the pomodoro animation

--- classes.py
+++ classes.py
@@ -25,9 +25,9 @@ class RGB:
         if factor < 0 or factor > 1: 
             raise ValueError("Factor must be between 0 and 1")
         return RGB(
-            r=int(self.red * factor), 
-            g=int(self.green * factor),
-            b=int(self.blue * factor)
+            red=int(self.red * factor), 
+            green=int(self.green * factor),
+            blue=int(self.blue * factor)
         )
 
 
@@ -43,5 +43,97 @@ class Colors:
     ORANGE = RGB(255, 165, 0)
 
 
+class LedStrip:
+    def __init__(self, num_leds: int = 10, frequency: int = 800, spi_device: str = '/dev/spidev0.0'):
+        self.num_leds = num_leds
+        self.frequency = frequency
+        self.spi_device = spi_device
+        self._neo = Pi5Neo(spi_device, num_leds, frequency)
+        self._state = [RGB() for _ in range(num_leds)]
+
+    @property
+    def state(self):
+        state_copy = self._state.copy()
+        return state_copy
+    
+    def set_led(self, index: int, rgb_color: RGB):
+        if index < 0 or index >= self.num_leds: 
+            raise IndexError("LED index out of range")
+        
+        self._state[index] = rgb_color
+        self._neo.set_led_color(index, rgb_color.red, rgb_color.green, rgb_color.blue)
+
+    def fill(self, color: RGB):
+        for i in range(self.num_leds):
+            self.set_led(i, color)
+
+    def update(self):
+        self._neo.update_strip()
+
+    def clear(self):
+        self.fill(Colors.OFF)
+        self.update()
+
+    def random_rgb_animation(self, leds_per_frame: int = 3, delay: float = 0.1):
+        """Displays random red, green or blue color accross the strip"""
+        n = self.num_leds
+        rgb = [Colors.RED, Colors.GREEN, Colors.BLUE]
+        try:
+            while True: 
+                colors = random.choices(rgb,k=leds_per_frame)
+                indices = random.sample(range(n), leds_per_frame)
+                for i in range(leds_per_frame):
+                    self.set_led(indices[i], colors[i])
+                self.update()
+                time.sleep(delay)
+        except KeyboardInterrupt:
+            self.clear()
+
+    def flashing_animation(self, delay: float = 0.3, duration: int = 15):
+        end_time = time.time() + duration
+        colors = [Colors.RED, Colors.GREEN, Colors.BLUE, Colors.WHITE, Colors.WARM_LIGHT, Colors.PINK, Colors.PURPLE, Colors.ORANGE]
+        try:
+            while time.time() < end_time:
+                color = random.choice(colors)
+                self.fill(color)
+                self.update()
+                time.sleep(delay)
+                self.clear()
+                time.sleep(delay)
+        except KeyboardInterrupt:
+            self.clear()
+        
+
+    def loading_animation(self, color:RGB = Colors.WARM_LIGHT, delay: float = 0.1):
+        
+        try:
+            while True: 
+                for i in range(self.num_leds):
+                    self.set_led(i, color)
+                    self.update()
+                    time.sleep(delay)
+                self.clear()
+        except KeyboardInterrupt: 
+            self.clear()
+
+    def pomodoro_animation(self, pomodoro_length: int = 25, color: RGB = Colors.BLUE):
+        try:
+            pace = pomodoro_length / self.num_leds # Time per LED in minutes
+            # Start pomodoro with all the lights on
+            self.fill(color = color)
+            self.update()
+            # Power off lights as time passes
+            dimmed_color = color.dim_color(0.2)
+            for i in range(self.num_leds - 1, -1, -1):
+                time.sleep(pace * 60) # Convert minutes to seconds
+                print(f"Turning off LED {i}, time passed: {pomodoro_length - pace * (self.num_leds - i)} minutes")
+                self.set_led(i, dimmed_color)
+                self.update()
+            # Pomodoro finished
+            self.flashing_animation()
+        except KeyboardInterrupt:
+            self.clear()
 
 print(type(Colors.RED))
+led_strip = LedStrip()
+print(led_strip.num_leds)
--- main.py
+++ main.py
@@ -2,44 +2,21 @@ from pi5neo import Pi5Neo
 import time
 import argparse 
 import random 
-from classes import Colors, RGB
+from classes import Colors, RGB, LedStrip
 
-
-neo = Pi5Neo('dev/spidev0.0', 10, 800)
-parser = argparse.ArgumentParser(description="LED loading bar")
-parser.add_argument("--color", "-c", default="day_light")
-
-
-def loading_bar(neo, color):
-    """Display a loading bar"""
-    r, g, b = get_rgb_value(color)
-    for i in range(neo.num_leds):
-        neo.set_led_color(i, r, g, b)
-        neo.update_strip()
-        time.sleep(0.1)
+def show_all_animations(strip: LedStrip):
+    strip.random_rgb_animation(leds_per_frame=3, delay=0.2)
+    strip.loading_animation()
+    strip.flashing_animation()
     
-
-def random_rgb(neo):
-    """Displays random rgb colors across the strip"""
-    # Get 3 random colors
-    # Change 3 random led within the range 
-    
-    while True: 
-        r_int = random.randint(0, 9)
-        r, g, b = get_random_color()
-        neo.set_led_color(r_int, r, g, b)
-        neo.update_strip()
-        time.sleep(0.2)
 
 
 def run():
-    user_color = parser.parse_args().color
-    user_color = user_color.upper()
-    day_light = getattr(Colors, user_color, Colors.WARM_LIGHT)
-    print(day_light)
-    neo.fill_strip(day_light.red, day_light.green, day_light.blue)
-    #neo.clear_strip()
-    neo.update_strip()
+    strip = LedStrip(num_leds = 10, frequency=800, spi_device='/dev/spidev0.0')
+    print(f"Initialized LED strip with {strip.num_leds} LEDs.")
+    #show_all_animations(strip)    
+    strip.pomodoro_animation(pomodoro_length=5)
+    print(strip.state)
 
 if __name__ == "__main__":
     run()
